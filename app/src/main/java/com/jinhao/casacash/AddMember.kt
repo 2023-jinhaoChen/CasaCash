25a508fcc66dd007b24843e638198806f1188824
guifre2003 <71527145+guifre2003@users.noreply.github.com>
2023-11-22 18:32:28 +0100

    Add addMember

---
+++ app/src/main/java/com/jinhao/casacash/AddMember.kt
@@ -0,0 +1,11 @@
+package com.jinhao.casacash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddMember : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_member)
+    }
+}
