1f20f6037ecfa21564bd74daa680802394aa36ea
JinhaoChenDAM <Jchen22@jaumebalmes.net>
2023-12-03 16:22:00 +0100

    Fix layout for activity_spending and activity_family_list

--- app/src/main/java/com/jinhao/casacash/FamilyListActivity.kt
+++ app/src/main/java/com/jinhao/casacash/FamilyListActivity.kt
@@ -3,6 +3,7 @@ package com.jinhao.casacash
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
