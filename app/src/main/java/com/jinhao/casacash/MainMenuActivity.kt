cd326544986ad6cd671ecba0396bf51cb2b55c11
guifre2003 <71527145+guifre2003@users.noreply.github.com>
2024-01-26 17:23:15 +0100

    Add custom menu

--- app/src/main/java/com/jinhao/casacash/MainMenuActivity.kt
+++ app/src/main/java/com/jinhao/casacash/MainMenuActivity.kt
@@ -6,10 +6,19 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainMenuActivity : AppCompatActivity(){
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
@@ -28,19 +37,44 @@ class MainMenuActivity : AppCompatActivity(){
             val intent = Intent(this, SpendingActivity::class.java)
             startActivity(intent)
         }
-    }
-    fun goToMainMenu(){
-        val intent = Intent(this, MainMenuActivity::class.java)
-        startActivity(intent)
-    }
-    fun goToSettings(){
-        val intent = Intent(this, SettingsActivity::class.java)
-        startActivity(intent)
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main_menu, menu)
-        return super.onCreateOptionsMenu(menu)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.navigation_view)
+
+// Configurar el icono del hamburguesa para abrir el menú
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, R.string.textAppName, R.string.textAppName
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+// Manejar clics en los elementos del menú de navegación
+        navView.setNavigationItemSelectedListener { menuItem ->
+            // Manejar las acciones del menú aquí
+            when (menuItem.itemId) {
+                R.id.item_change_password -> {
+                    goToChangePassword()
+                    true
+                }
+                R.id.item_manage_family -> {
+                    goToManageFamily()
+                    true
+                }
+                R.id.item_family_request -> {
+                    goToFamilyRequest()
+                    true
+                }
+                R.id.item_logout -> {
+                    goToLogin()
+                    true
+                }
+
+                else -> false
+            }
+        }
+
+        val btnOpenMenu: ImageButton = findViewById(R.id.tb_menu)
+        btnOpenMenu.setOnClickListener { openMenu(it) }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -51,5 +85,52 @@ class MainMenuActivity : AppCompatActivity(){
             R.id.item_logout -> Toast.makeText(this, "logout", Toast.LENGTH_SHORT).show()
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    fun goToChangePassword(){
+        val intent = Intent(this, NewPasswordActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun goToManageFamily(){
+        val intent = Intent(this, FamilyActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun goToFamilyRequest(){
+        val intent = Intent(this, RequestsActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun goToLogin(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun openMenu(view: View) {
+        if (!drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+    }
+    fun goToMainMenu(view: View){
+        val intent = Intent(this, MainMenuActivity::class.java)
+        startActivity(intent)
+    }
+    fun goToMainMenu(){
+        val intent = Intent(this, MainMenuActivity::class.java)
+        startActivity(intent)
+    }
+    fun goToSettings(view: View){
+        val intent = Intent(this, SettingsActivity::class.java)
+        startActivity(intent)
+    }
+    fun goToSettings(){
+        val intent = Intent(this, SettingsActivity::class.java)
+        startActivity(intent)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
